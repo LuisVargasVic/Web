7cfc9f00fcfc964f3319c260cb25e41791f4b7d0
Luis Vargas <luisev96@gmail.com>
2019-02-19 17:31:18 -0600

    [Web] Pages to server.go

---
+++ Project/server/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+)
+
+func showIndex(w http.ResponseWriter, r *http.Request) {
+	css := http.FileServer(http.Dir("../css"))
+	http.Handle("/css/", http.StripPrefix("/css/", css))
+	js := http.FileServer(http.Dir("../vendor"))
+	http.Handle("/vendor/", http.StripPrefix("/vendor/", js))
+	im := http.FileServer(http.Dir("../img"))
+	http.Handle("/img/", http.StripPrefix("/img/", im))
+	pg := http.FileServer(http.Dir("../pages"))
+	http.Handle("/pages/", http.StripPrefix("/pages/", pg))
+	http.ServeFile(w, r, "../pages/index.html")
+}
+
+func showAbout(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../pages/about.html")
+}
+
+func showCatalog(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../pages/catalog.html")
+}
+
+func showService(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../pages/service.html")
+}
+
+func main() {
+
+	http.HandleFunc("/index", showIndex)
+	http.HandleFunc("/about", showAbout)
+	http.HandleFunc("/catalog", showCatalog)
+	http.HandleFunc("/service", showService)
+	http.ListenAndServe(":8080", nil)
+
+}
