a891aab6cae20a2ccac76b73d0e2c6d2c5e10629
Luis Vargas <luisev96@gmail.com>
2019-02-19 21:17:28 -0600

    [Web] Firebase added

--- Project/server/server.go
+++ Project/server/server.go
@@ -5,14 +5,6 @@ import (
 )
 
 func showIndex(w http.ResponseWriter, r *http.Request) {
-	css := http.FileServer(http.Dir("../css"))
-	http.Handle("/css/", http.StripPrefix("/css/", css))
-	js := http.FileServer(http.Dir("../vendor"))
-	http.Handle("/vendor/", http.StripPrefix("/vendor/", js))
-	im := http.FileServer(http.Dir("../img"))
-	http.Handle("/img/", http.StripPrefix("/img/", im))
-	pg := http.FileServer(http.Dir("../pages"))
-	http.Handle("/pages/", http.StripPrefix("/pages/", pg))
 	http.ServeFile(w, r, "../pages/index.html")
 }
 
@@ -29,11 +21,19 @@ func showService(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
+	css := http.FileServer(http.Dir("../css"))
+	http.Handle("/css/", http.StripPrefix("/css/", css))
+	js := http.FileServer(http.Dir("../vendor"))
+	http.Handle("/vendor/", http.StripPrefix("/vendor/", js))
+	im := http.FileServer(http.Dir("../img"))
+	http.Handle("/img/", http.StripPrefix("/img/", im))
+	pg := http.FileServer(http.Dir("../pages"))
+	http.Handle("/pages/", http.StripPrefix("/pages/", pg))
 	http.HandleFunc("/index", showIndex)
 	http.HandleFunc("/about", showAbout)
 	http.HandleFunc("/catalog", showCatalog)
 	http.HandleFunc("/service", showService)
 	http.ListenAndServe(":8080", nil)
+	
 
 }
